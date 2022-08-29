252c41377612123427e60c4cf2b22d79f9dd958c
maxhager <maxhager28@gmail.com>
2022-08-29 10:15:03 +0200

    Update

--- README.md
+++ README.md
@@ -1,8 +1,15 @@
 # MaxBook
 
+## What 
+
+Personal homepage where I start with tracking my projects.
+
 ## Why
 
-1. More success in finishing my projects. If I write something down the probabilies are higher to stick them (personal experience and Cal Newport)
+1. More success in finishing my projects. If I write something down the probabilies are higher to stick them (personal experience and Cal Newport).
 
 2. Online reference. The personal book site is my personal digital garden and I can make all kinds of things public.
 
+## Dependencies
+
+`pip install Markdown`
--- converter.py
+++ converter.py
@@ -3,6 +3,34 @@
 2. render htmkl and check if its the format which i wish to have
 '''
 import markdown
+from git import Repo
+# https://www.devdungeon.com/content/convert-markdown-html-python tutorial about Markdown Python
+
 
 def convert():
-    markdown.markdown("")
+    markdown.markdownFromFile(
+        input='home.md',
+        output='index.html',
+        extensions=['toc'],
+    )
+
+
+def pushToGithub():
+    repo_dir = ''
+    repo = Repo(repo_dir)
+    file_list = [
+        "converter.py",
+        "index.html",
+        "home.md",
+        "README.md"
+    ]
+    commit_message = 'Update'
+    repo.index.add(file_list)
+    repo.index.commit(commit_message)
+    origin = repo.remote('origin')
+    origin.push()
+
+
+if __name__ == "__main__":
+    convert()
+    pushToGithub()
