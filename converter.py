3abd6c1a573636b3bd89384e28ed0b7404779f00
Max Hager <maxhager28@gmail.com>
2022-10-04 00:00:01 +0200

    update

--- converter.py
+++ converter.py
@@ -1,11 +1,4 @@
-'''
-1. m for converting md to hmtl
-2. render htmkl and check if its the format which i wish to have
-'''
 import markdown
-from git import Repo
-import sys
-import os
 import subprocess
 import config
 # https://www.devdungeon.com/content/convert-markdown-html-python tutorial about Markdown Python
@@ -20,26 +13,9 @@ def convert():
 
 
 def pushToGithub():
-    #some new comment
     subprocess.call(['git', 'add', '.'], cwd='/Users/maxhager/Projects2022/MaxBook')
     subprocess.call(['git', 'commit', '-m', 'update'], cwd='/Users/maxhager/Projects2022/MaxBook')
     subprocess.call(['git', 'push', 'https://{}@github.com/yachty66/MaxBook.git'.format(config.tokenGit)], cwd='/Users/maxhager/Projects2022/MaxBook')
-    '''repo_dir = '/Users/maxhager/Projects2022/MaxBook'
-    repo = Repo(repo_dir)
-    file_list = [
-        "/Users/maxhager/Projects2022/MaxBook/converter.py",-
-        "/Users/maxhager/Projects2022/MaxBook/index.html",-
-        "/Users/maxhager/Projects2022/MaxBook/home.md",-
-        "/Users/maxhager/Projects2022/MaxBook/README.md",
-        "/Users/maxhager/Projects2022/MaxBook/DailyMe/dailyMe.html",
-        "/Users/maxhager/Projects2022/MaxBook/DailyMe/images/"
-    ]
-    commit_message = 'Update'
-    repo.index.add(file_list)
-    repo.index.commit(commit_message)
-    origin = repo.remote('origin')
-    origin.push()'''
-
 
 if __name__ == "__main__":
     convert()
