d9221ecd85a7aa4b0d56bd6afecb335807120a11
Max Hager <maxhager28@gmail.com>
2022-10-03 20:48:01 +0200

    Update

--- converter.py
+++ converter.py
@@ -19,7 +19,7 @@ def convert():
 
 
 def pushToGithub():
-    subprocess.call(['git', 'add', '/Users/maxhager/Projects2022/MaxBook/converter.py', '/Users/maxhager/Projects2022/MaxBook/index.html', '/Users/maxhager/Projects2022/MaxBook/home.md', '/Users/maxhager/Projects2022/MaxBook/README.md', '/Users/maxhager/Projects2022/MaxBook/DailyMe/dailyMe.html', '/Users/maxhager/Projects2022/MaxBook/DailyMe/images/'], cwd='/Users/maxhager/Projects2022/MaxBook')
+    subprocess.call(['git', 'add', '.'], cwd='/Users/maxhager/Projects2022/MaxBook')
     subprocess.call(['git', 'commit', '-m', '{}'.format("Update")], cwd='/Users/maxhager/Projects2022/MaxBook')
     subprocess.call(['git', 'push', 'https://{}@github.com/yachty66/MaxBook.git'.format(config.tokenGit)], cwd='/Users/maxhager/Projects2022/MaxBook')
     '''repo_dir = '/Users/maxhager/Projects2022/MaxBook'
